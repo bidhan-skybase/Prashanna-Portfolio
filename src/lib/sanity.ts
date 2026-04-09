cd4bb24e38e21876ac70b2661ca4d7180f4f5bc1
bidhan-skybase <bidhan@skybase.com.np>
2026-04-09 18:48:39 +0545

    chore: Remove sanity config

--- src/lib/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: "42a7ekko",
-  dataset: "production",
-  useCdn: true,
-  apiVersion: "2024-01-01",
-});
