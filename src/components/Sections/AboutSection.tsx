7be09fb92742e373f06158447569a354a48e849d
bidhan-skybase <bidhan@skybase.com.np>
2026-08-24 18:49:32 +0545

    feat: Update co-founder to founder

--- src/components/Sections/AboutSection.tsx
+++ src/components/Sections/AboutSection.tsx
@@ -66,7 +66,7 @@ const AboutSection = () => {
                                         fontFamily: '"Helvetica Neue", Arial, sans-serif',
                                     }}
                                 >
-                                    In 2021, he co-founded{" "}
+                                    In 2021, he founded{" "}
                                     <a
                                         href="https://www.instagram.com/untitled.np?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D"
                                         target="_blank"
